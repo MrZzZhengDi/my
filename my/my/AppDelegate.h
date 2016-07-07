e95020a0ddcc4a38cc5c610ea4451b07d4b915c6
MrZz <347214109@qq.com>
2016-07-07 18:26:10 +0800

    测试

---
+++ my/my/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  my
+//
+//  Created by MrZz on 16/7/7.
+//  Copyright © 2016年 MrZz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ my/my/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  my
+//
+//  Created by MrZz on 16/7/7.
+//  Copyright © 2016年 MrZz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
